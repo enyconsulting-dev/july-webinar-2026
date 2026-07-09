466aceae8a4b5545f6b0d7795586d7f02eed84e9
Obed-Ojingwa <obedojingwa@gmail.com>
2026-07-09 06:00:15 +0100

    Pathlinking

---
+++ frontend/src/main.tsx
@@ -0,0 +1,34 @@
+// /home/obed/Documents/free-webinar-sales/frontend/src/main.tsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import RootLayout from "./RootLayout";
+import OptInPage from "./pages/OptInPage";
+import ThankYouPage from "./pages/ThankYouPage";
+import VipCheckoutPage from "./pages/VipCheckoutPage";
+import VipThankYouPage from "./pages/VipThankYouPage";
+import PlatinumCheckoutPage from "./pages/PlatinumCheckoutPage";
+import PlatinumThankYouPage from "./pages/PlatinumThankYouPage";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <OptInPage /> },
+      { path: "/thank-you", element: <ThankYouPage /> },
+      { path: "/vip-checkout", element: <VipCheckoutPage /> },
+      { path: "/vip-confirmed", element: <VipThankYouPage /> },
+      { path: "/platinum-checkout", element: <PlatinumCheckoutPage /> },
+      { path: "/platinum-confirmed", element: <PlatinumThankYouPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
